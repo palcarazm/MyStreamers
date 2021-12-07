b67233bbace701e53602a32a34bfc8d8a555b308
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2021-12-07 18:57:08 +0100

    Corrección de errores de render
    Signed-off-by: Pablo Alcaraz Martínez <pablo@alcarazm.es>

--- dist/views/config/config.php
+++ dist/views/config/config.php
@@ -198,12 +198,14 @@
         <!-- Fin siteConfig -->
         <!-- Inicio finalMessage -->
         <li class="list-group-item step todo" id="finalMessage">
-            <div class="alert alert-success step-body" role="alert">
-                <h4 class="alert-heading">Bien hecho!</h4>
-                <p>Has finalizado la configuración inicial de My Streamers. Ahora ya tu web y base de datos se encuentran conectadas, has creado el administrador del sistema y configurado los datos básicos de tu sitio.</p>
-                <p class="mb-0">¿Por qué no te pasas por el <a href="#" class="alert-link text-white">panel de administración</a> para añadir información a tu sitio?</p>
+            <div class="step-body">
+                <div class="alert alert-success step-body" role="alert">
+                    <h4 class="alert-heading">Bien hecho!</h4>
+                    <p>Has finalizado la configuración inicial de My Streamers. Ahora ya tu web y base de datos se encuentran conectadas, has creado el administrador del sistema y configurado los datos básicos de tu sitio.</p>
+                    <p class="mb-0">¿Por qué no te pasas por el <a href="#" class="alert-link text-white">panel de administración</a> para añadir información a tu sitio?</p>
+                </div>
+                <p><span class="badge badge-warning"><i class="fas fa-exclamation-circle  mr-1"></i>Por seguridad, vamos a bloquear el acceso a esta sección para que nadie pueda modificar la configuración del sitio sin estar autenticado.</span></p>
             </div>
-            <p><span class="badge badge-warning"><i class="fas fa-exclamation-circle  mr-1"></i>Por seguridad, vamos a bloquear el acceso a esta sección para que nadie pueda modificar la configuración del sitio sin estar autenticado.</span></p>
         </li>
         <!-- Fin finalMessage -->
     </ul>
