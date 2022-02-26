07ae4a0b323750a3023ab16d02cee1b86be7e574
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2022-02-26 10:11:10 +0100

    refactor: Metodo de llamada AJAX
    - Añadido método generico de llamada a API
    - Refactorizacion del método de llamada a API verbose

--- src/js/global/main.js
+++ src/js/global/main.js
@@ -4,14 +4,17 @@
  * @param {String} method Tipo de solicitud
  * @param {FormData} formdata Datos a enviar
  * @param {int} requiered Valores requeridos
+ * @param {int} verbose Tipo de llamada 0--> No verbose, 1 --> solo validacion, 2 --> Validacion y error, 3 --> Verbose
  * @param {function} success función a ejecutar en caso de éxito
  * @param {function} error función a ejecutar en caso de error
+ * @returns
  */
-function callAPIverbose(
+function callAPI(
   uri,
   method,
   formdata,
   requiered,
+  verbose = 1,
   success = null,
   error = null
 ) {
@@ -19,22 +22,26 @@ function callAPIverbose(
   formdata.forEach((input) => {
     length++;
     if (input == null || input == "") {
+      if (verbose > 0) {
+        swal({
+          type: "warning",
+          title: "Error",
+          html: "Todos los campos son obligatorios",
+          timer: 3000,
+        });
+      }
+      return false;
+    }
+  });
+  if (length < requiered) {
+    if (verbose > 0) {
       swal({
         type: "warning",
         title: "Error",
         html: "Todos los campos son obligatorios",
         timer: 3000,
       });
-      return false;
     }
-  });
-  if (length < requiered) {
-    swal({
-      type: "warning",
-      title: "Error",
-      html: "Todos los campos son obligatorios",
-      timer: 3000,
-    });
     return false;
   }
   $.ajax({
@@ -49,39 +56,77 @@ function callAPIverbose(
     success: function (data) {
       //console.log(data);
       if (success == null) {
-        swal({
-          type: "success",
-          title: "Éxito",
-          html: data.message,
-          timer: 2000,
-        });
+        if (verbose > 2) {
+          swal({
+            type: "success",
+            title: "Éxito",
+            html: data.responseJSON.message,
+            timer: 2000,
+          });
+        }
       } else {
-        swal({
-          type: "success",
-          title: "Éxito",
-          html: data.message,
-          timer: 2000,
-        }).then(() => success());
+        if (verbose > 2) {
+          swal({
+            type: "success",
+            title: "Éxito",
+            html: data.responseJSON.message,
+            timer: 2000,
+          }).then(() => success());
+        } else {
+          success();
+        }
       }
     },
     error: function (data) {
       //console.log(data);
       if (error == null) {
-        swal({
-          type: "error",
-          title: "Error",
-          html: data.message,
-        });
+        if (verbose > 1) {
+          swal({
+            type: "error",
+            title: "Error",
+            html: data.responseJSON.message,
+          });
+        }
       } else {
-        swal({
-          type: "error",
-          title: "Error",
-          html: data.message,
-        }).then(() => error());
+        if (verbose > 1) {
+          swal({
+            type: "error",
+            title: "Error",
+            html: data.responseJSON.message,
+          }).then(() => error());
+        } else {
+          error();
+        }
       }
     },
   });
   return true;
+}
+
+/**
+ * Llama a un api y muestra alertas en función del estado
+ * @param {String} uri Dirección de la APi
+ * @param {String} method Tipo de solicitud
+ * @param {FormData} formdata Datos a enviar
+ * @param {int} requiered Valores requeridos
+ * @param {function} success función a ejecutar en caso de éxito
+ * @param {function} error función a ejecutar en caso de error
+ */
+function callAPIverbose(
+  uri,
+  method,
+  formdata,
+  requiered,
+  success = null,
+  error = null
+) {
+  return callAPI( uri,
+    method,
+    formdata,
+    requiered,
+    3,
+    success,
+    error)
 }
 /** Devuelve la URL de la página actual
  *
@@ -99,20 +144,22 @@ function currentURL() {
  */
 jQuery.fn.loading = function () {
   return this.css("position", "relative").append(
-    $('<div></div>').addClass("overlay loading").append(
-      $('<i></i>').addClass('fas fa-spinner fa-spin-ease')
-    )
+    $("<div></div>")
+      .addClass("overlay loading")
+      .append($("<i></i>").addClass("fas fa-spinner fa-spin-ease"))
   );
 };
 /**
  * Elimina el overlay de carga
  */
- jQuery.fn.loaded = function () {
-  return this.find('.overlay.loading').remove();
+jQuery.fn.loaded = function () {
+  return this.find(".overlay.loading").remove();
 };
 /**
  * Intercambia los estados de carga
  */
- jQuery.fn.loadToggle = function() {
-  return this.has('.overlay.loading').length > 0 ? this.loaded() : this.loading();
+jQuery.fn.loadToggle = function () {
+  return this.has(".overlay.loading").length > 0
+    ? this.loaded()
+    : this.loading();
 };
