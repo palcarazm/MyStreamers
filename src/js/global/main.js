a768e7de97fbce557503bf831e7acc95d6cbf28a
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2022-04-15 13:48:39 +0200

    feat: soporte para envío de archivos vía AJAX

--- src/js/global/main.js
+++ src/js/global/main.js
@@ -21,8 +21,13 @@ function callAPI(
   authorization = null
 ) {
   let length = 0;
-  var object = {};
-  formdata.forEach((value,input) => {
+  var data = {};
+  var waiting = false;
+  var file;
+  var metadata;
+  var file_input;
+
+  formdata.forEach((value, input) => {
     length++;
     if (input == null || input == "") {
       if (verbose > 0) {
@@ -35,8 +40,22 @@ function callAPI(
       }
       return false;
     }
-    object[input] = value;
+    if (typeof value == "object") {
+      if (value["size"] > 0) {
+        file = value;
+        metadata = {
+          name: value["name"],
+          type: value["type"],
+          size: value["size"],
+        };
+        file_input = input;
+        waiting = true;
+      }
+    } else {
+      data[input] = value;
+    }
   });
+
   if (length < requiered) {
     if (verbose > 0) {
       swal({
@@ -48,68 +67,110 @@ function callAPI(
     }
     return false;
   }
-  $.ajax({
-    type: method,
-    data: JSON.stringify(object),
-    url: uri,
-    dataType: "json",
-    contentType: false,
-    processData: false,
-    async: true,
-    cache: false,
-    success: function (data) {
-      //console.log(data);
-      if (success == null) {
-        if (verbose > 2) {
-          swal({
-            type: "success",
-            title: "Éxito",
-            html: data.message,
-            timer: 2000,
-          });
-        }
-      } else {
-        if (verbose > 2) {
-          swal({
-            type: "success",
-            title: "Éxito",
-            html: data.message,
-            timer: 2000,
-          }).then(() => success());
+  if (!waiting) {
+    ajaxCall(uri, method, data, verbose, success, error, authorization);
+  } else {
+    readUploadedFileAsBinary(
+      file,
+      metadata,
+      file_input,
+      uri,
+      method,
+      data,
+      verbose,
+      success,
+      error,
+      authorization
+    );
+  }
+}
+
+/**
+ * Lanza la llamada Ajax y muestra alertas en función del estado
+ * @param {String} uri Dirección de la APi
+ * @param {String} method Tipo de solicitud
+ * @param {Object} data Datos a enviar
+ * @param {int} verbose Tipo de llamada 0--> No verbose, 1 --> solo validacion, 2 --> Validacion y error, 3 --> Verbose
+ * @param {function} success función a ejecutar en caso de éxito
+ * @param {function} error función a ejecutar en caso de error
+ * @param {string} authorization cadena de autorización para incorporar al header
+ * @returns
+ */
+function ajaxCall(
+  uri,
+  method,
+  data,
+  verbose = 1,
+  success = null,
+  error = null,
+  authorization = null
+) {
+  try {
+    $.ajax({
+      type: method,
+      data: JSON.stringify(data),
+      url: uri,
+      dataType: "json",
+      contentType: false,
+      processData: false,
+      async: true,
+      cache: false,
+      success: function (data) {
+        //console.log(data);
+        if (success == null) {
+          if (verbose > 2) {
+            swal({
+              type: "success",
+              title: "Éxito",
+              html: data.message,
+              timer: 2000,
+            });
+          }
         } else {
-          success();
+          if (verbose > 2) {
+            swal({
+              type: "success",
+              title: "Éxito",
+              html: data.message,
+              timer: 2000,
+            }).then(() => success());
+          } else {
+            success();
+          }
         }
-      }
-    },
-    error: function (data) {
-      //console.log(data);
-      if (error == null) {
-        if (verbose > 1) {
-          swal({
-            type: "error",
-            title: "Error",
-            html: data.responseJSON.message,
-          });
-        }
-      } else {
-        if (verbose > 1) {
-          swal({
-            type: "error",
-            title: "Error",
-            html: data.responseJSON.message,
-          }).then(() => error());
+      },
+      error: function (data) {
+        //console.log(data);
+        if (error == null) {
+          if (verbose > 1) {
+            swal({
+              type: "error",
+              title: "Error",
+              html: data.responseJSON.message,
+            });
+          }
         } else {
-          error();
+          if (verbose > 1) {
+            swal({
+              type: "error",
+              title: "Error",
+              html: data.responseJSON.message,
+            }).then(() => error());
+          } else {
+            error();
+          }
         }
-      }
-    },
-    beforeSend: function (xhr) {
-      if (authorization != null) {
-        xhr.setRequestHeader('Authorization', authorization);
-      }
-    },
-  });
-  return true;
+      },
+      beforeSend: function (xhr) {
+        if (authorization != null) {
+          xhr.setRequestHeader("Authorization", authorization);
+        }
+      },
+    });
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 /**
@@ -129,14 +190,43 @@ function callAPIverbose(
   success = null,
   error = null
 ) {
-  return callAPI( uri,
-    method,
-    formdata,
-    requiered,
-    3,
-    success,
-    error)
+  return callAPI(uri, method, formdata, requiered, 3, success, error);
 }
+
+/**
+ * Lee un fichero y lo devuelve en binario
+ * @param {Object} file Fichero que leer
+ * @param {Object} metada Datos del fichero que leer
+ * @param {String} input entrada que realizar en datos
+ * @param {String} uri Dirección de la APi
+ * @param {String} method Tipo de solicitud
+ * @param {FormData} data Datos a enviar
+ * @param {int} requiered Valores requeridos
+ * @param {function} success función a ejecutar en caso de éxito
+ * @param {function} error función a ejecutar en caso de error
+ * @returns {String} fichero en binario
+ */
+function readUploadedFileAsBinary(
+  file,
+  metadata,
+  input,
+  uri,
+  method,
+  data,
+  verbose,
+  success,
+  error,
+  authorization
+) {
+  var reader = new FileReader();
+  reader.onload = () => {
+    metadata["content"] = reader.result;
+    data[input] = metadata;
+    ajaxCall(uri, method, data, verbose, success, error, authorization);
+  };
+  reader.readAsDataURL(file);
+}
+
 /** Devuelve la URL de la página actual
  *
  * @returns URL actual
