28bd61a30d03209618c7891a759c22e73844b9f8
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2022-05-08 15:47:29 +0200

    apertura de rutas

--- dist/public/index.php
+++ dist/public/index.php
@@ -47,6 +47,13 @@ $router->add('PUT','/api/user/v1/user',[UserApi::class,'putUser']);
 $router->add('DELETE','/api/user/v1/user',[UserApi::class,'deleteUser']);
 $router->add('PATCH','/api/user/v1/user/lock',[UserApi::class,'lockUser']);
 $router->add('PATCH','/api/user/v1/user/unlock',[UserApi::class,'unlockUser']);
+$router->add('POST','/api/user/v1/link',[UserApi::class,'postLink']);
+$router->add('PUT','/api/user/v1/link',[UserApi::class,'putLink']);
+$router->add('DELETE','/api/user/v1/link',[UserApi::class,'deleteLink']);
+$router->add('PUT','/api/user/v1/profile/links',[UserApi::class,'putProfileLinks']);
+$router->add('POST','/api/user/v1/profile',[UserApi::class,'postProfile']);
+$router->add('PATCH','/api/user/v1/profile/lock',[UserApi::class,'lockProfile']);
+$router->add('PATCH','/api/user/v1/profile/unlock',[UserApi::class,'unlockProfile']);
 
 // Administración
 /// Perfil
@@ -57,9 +64,14 @@ $router->add('GET','/admin/config/sitio',[AdminController::class,'configSitio'],
 $router->add('GET','/admin/usuarios/crear',[AdminController::class,'userAdd'],Rol::PERMS_USUARIOS);
 $router->add('GET','/admin/usuarios/editar',[AdminController::class,'userEdit'],Rol::PERMS_USUARIOS);
 $router->add('GET','/admin/usuarios/listar',[AdminController::class,'userList'],Rol::PERMS_USUARIOS);
+$router->add('GET','/admin/usuarios/enlaces/crear',[AdminController::class,'linkAdd'],Rol::PERMS_USUARIOS);
+$router->add('GET','/admin/usuarios/enlaces/editar',[AdminController::class,'linkEdit'],Rol::PERMS_USUARIOS);
+$router->add('GET','/admin/usuarios/enlaces/listar',[AdminController::class,'linkList'],Rol::PERMS_USUARIOS);
 
 // Publicas
 $router->add('GET','/',[PublicController::class, 'index']);
+$router->add('GET','/participantes',[PublicController::class, 'participantes']);
+$router->add('GET','/participantes/ficha',[PublicController::class, 'participante']);
 
 /* ENROUTAR */
 if (is_file(__DIR__ . '/../config/config.php')) {
